acf28d374ad1f561aa0b59df79a3d5ead759890c
csj8520 <2372138557@qq.com>
2021-11-19 07:50:50 +0000

    🐛 fix: Cannot find module 'node:vm'

--- utils/import-module.js
+++ utils/import-module.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
 const got_1 = (0, tslib_1.__importDefault)(require("got"));
-const node_vm_1 = (0, tslib_1.__importDefault)(require("node:vm"));
+const vm_1 = (0, tslib_1.__importDefault)(require("vm"));
 async function importModule(...urls) {
     for (const it of urls) {
         let error = false;
@@ -10,7 +10,7 @@ async function importModule(...urls) {
             if (/^http(s)?:\/\//.test(it)) {
                 const notify = await got_1.default.get(it);
                 const _module = { ...module, exports: {} };
-                const func = node_vm_1.default.runInThisContext(`(function(exports, require, module, __filename, __dirname){${notify.body}})`);
+                const func = vm_1.default.runInThisContext(`(function(exports, require, module, __filename, __dirname){${notify.body}})`);
                 func.call(_module.exports, _module.exports, require, _module, __filename, __dirname);
                 return _module.exports;
                 // return eval(notify.body);
