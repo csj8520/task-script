5ae8d29894bb285d5670e46ab5b57d39ec1ba46a
csj8520 <2372138557@qq.com>
2021-12-03 03:37:11 +0000

    📦 添加免费菜领取提示

--- task-jx_cjsqk.js
+++ task-jx_cjsqk.js
@@ -1,13 +1,16 @@
 "use strict";
 /**
  * 京喜超级省钱卡
- * 每周日执行
+ * 每日执行   提醒领取免费菜
+ * 每周日执行 领取优惠券
  * 脚本兼容: Node.js
- * cron 30 8,10 * * 0 task-jx_cjsqk.js
+ * cron 0,30 21 * * * task-jx_cjsqk.js
  * new Env('京喜超级省钱卡')
  *
  * 环境变量
  * JD_COOKIE           京东cookie
+ *
+ * 每日免费菜 每日22点重置领取资格
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 const tslib_1 = require("tslib");
@@ -21,6 +24,7 @@ if (!process.env.JD_COOKIE) {
 }
 const DEBUG = process.env.DEBUG === 'true';
 const cookies = process.env.JD_COOKIE.split('&');
+let needSendNotify = false;
 const init = async ({ cookie, index }) => {
     const options = {
         headers: {
@@ -35,7 +39,17 @@ const init = async ({ cookie, index }) => {
     let userRights = await got_1.default.get('https://m.jingxi.com/ppvip/ppvip_rights/QueryUserRights?sceneval=2', options).json();
     DEBUG && console.log('userRights: ', JSON.stringify(userRights));
     // TODO: freeMenu 每日免费菜
-    log.log(userRights.freeMenu.tips.noChanceTips);
+    log.log(`每日免费菜：${userRights.freeMenu.tips.noChanceTips}`);
+    for (let food of userRights.freeMenu.menus) {
+        if (food.status === '1') {
+            needSendNotify = true;
+            log.log(`${food.skuName}：可免费领取`);
+            // const receiveFood: any = await got
+            //   .get(`https://m.jingxi.com/ppvip/ppvip_rights/GetFreeMenuCoupon?skuId=${food.skuId}&type=1&_stk=skuId%2Ctype&sceneval=2`, options)
+            //   .json();
+            // console.log(receiveFood);
+        }
+    }
     log.log('');
     // IGNORE: couponLife 生活特惠
     const isSunday = new Date().getDay() === 0;
@@ -47,6 +61,7 @@ const init = async ({ cookie, index }) => {
         for (const quan of userRights.couponLong) {
             if (quan.status === '1')
                 continue;
+            needSendNotify = true;
             const url = `https://m.jingxi.com/ppvip/ppvip_rights/GetVIPCoupon?token=${quan.token}&couponType=2&_stk=_t%2CcouponType%2Ctoken&sceneval=2`;
             const quanStatus = await got_1.default.get(url, options).json();
             DEBUG && console.log('quanStatus: ', quanStatus);
@@ -77,7 +92,9 @@ const init = async ({ cookie, index }) => {
         log.log(e);
     }
     finally {
-        const { sendNotify } = await (0, utils_1.importModule)(path_1.default.resolve('./sendNotify.js'), (0, utils_1.cdn)('https://raw.githubusercontent.com/he1pu/JDHelp/main/sendNotify.js'), 'https://raw.githubusercontent.com/he1pu/JDHelp/main/sendNotify.js');
-        await sendNotify('京喜超级省钱卡', log.logs.join('\n'));
+        if (needSendNotify) {
+            const { sendNotify } = await (0, utils_1.importModule)(path_1.default.resolve('./sendNotify.js'), (0, utils_1.cdn)('https://raw.githubusercontent.com/he1pu/JDHelp/main/sendNotify.js'), 'https://raw.githubusercontent.com/he1pu/JDHelp/main/sendNotify.js');
+            await sendNotify('京喜超级省钱卡', log.logs.join('\n'));
+        }
     }
 })();
